1e27eed4c08a060f34ebc7527062395447ae5639
moonslight2340 <moonslight2340@gmail.com>
2026-09-06 02:17:00 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Setelah Worker online, ganti nilai ini dengan URL Worker kamu.
 // Contoh: https://xi-akl-1-api.nama-subdomain.workers.dev
-window.AKL_API_URL = "https://GANTI-DENGAN-URL-WORKER-ANDA";
+window.AKL_API_URL = "https://xi-akl-1-api.moonslight2340.workers.dev/";
